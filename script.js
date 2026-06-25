991b00000c36ca25edfbb91d975f5827208ae523
rinatmolorov777-spec <rinatmolorov777@gmail.com>
2026-06-25 15:25:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    alert("Ринаттын резюмесине кош келиңиз!");
+});
+
+function showMessage() {
+    alert("Максатым: Келечекте өз кесибимдин мыкты адиси болуу!");
+}
